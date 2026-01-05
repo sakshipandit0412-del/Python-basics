d9bc449f17a83590171d7dd40ce90709b2e99464
sakshipandit0412-del <sakshipandit0412@gmail.com>
2026-01-05 17:29:47 +0530

    Add files via upload

---
+++ pydemo5.py
@@ -0,0 +1,22 @@
+myset = {"apple", "banana", "cherry"}
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry", False, True, 0}
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset))
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
