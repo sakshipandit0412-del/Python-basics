a2707fe6826aa09fcc0dc21f338180d27d9cc8f9
sakshipandit0412-del <sakshipandit0412@gmail.com>
2026-01-06 10:00:41 +0530

    Add files via upload

---
+++ pydemo11.py
@@ -0,0 +1,25 @@
+import math
+from datetime import date
+from datetime import datetime
+math.sqrt(4) 
+
+from math import sqrt       
+sqrt(4)         
+
+math.log10(100)
+
+math.sqrt(60)
+
+math.pi
+
+print(math.sin(math.radians(90)))
+
+print(date.today())
+
+today = date.today()
+print(today)
+
+print(today.day, today.month, today.year)
+
+
+print(datetime.now())   
